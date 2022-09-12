b874ac3ce284f77b3438c0e45c03c19701f970ca
su-de-sh <sudesh7443@gmail.com>
2022-09-12 16:36:55 +0545

    feat: add comment to each blog

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 // import { UserInfo } from "./UserInfo";
 
 const BlogDetail = ({ blog, updateLike }) => {
+  if (!blog) return null;
+
+  const [comments, setComments] = useState([]);
   const increaseLike = (id) => {
     // console.log("id", id);
     updateLike(id, blog.likes + 1);
+  };
+
+  // const comments = ["first comment", "second comments"];
+  const handleComment = (event) => {
+    event.preventDefault();
+    const comment = event.target.comment.value;
+    setComments([...comments, comment]);
+    event.target.comment.value = "";
   };
   return (
     <div>
@@ -22,6 +33,18 @@ const BlogDetail = ({ blog, updateLike }) => {
         </button>
       </div>
       added by {blog.author}
+      <div style={{ marginTop: "10px", marginBottom: "20px" }}>
+        <strong>comments</strong>
+        <form onSubmit={handleComment}>
+          <input type="text" name="comment" />
+          <button>add comment</button>
+        </form>
+
+        {comments &&
+          comments.map((comment, index) => {
+            return <li key={index}>{comment}</li>;
+          })}
+      </div>
     </div>
   );
 };
