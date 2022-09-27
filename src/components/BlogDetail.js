08761d3efc748368d784a6abdc4039dc002e5454
su-de-sh <sudesh7443@gmail.com>
2022-09-27 20:58:13 +0545

    feat: featch the comments of the blog

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 // import { UserInfo } from "./UserInfo";
 
 const BlogDetail = ({ blog, updateLike }) => {
   if (!blog) return null;
 
   const [comments, setComments] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/api/blogs/${blog.id}/comments`)
+      .then((result) => {
+        setComments(result.data);
+      });
+  }, []);
   const increaseLike = (id) => {
     // console.log("id", id);
     updateLike(id, blog.likes + 1);
   };
 
   // const comments = ["first comment", "second comments"];
-  const handleComment = (event) => {
+  const handleComment = async (event) => {
     event.preventDefault();
     const comment = event.target.comment.value;
-    setComments([...comments, comment]);
+
+    axios
+      .post(`http://localhost:3001/api/blogs/${blog.id}/comments`, { comment })
+      .then((result) => {
+        setComments([...comments, result.data]);
+      });
     event.target.comment.value = "";
   };
   return (
@@ -42,7 +55,7 @@ const BlogDetail = ({ blog, updateLike }) => {
 
         {comments &&
           comments.map((comment, index) => {
-            return <li key={index}>{comment}</li>;
+            return <li key={index}>{comment.comment}</li>;
           })}
       </div>
     </div>
